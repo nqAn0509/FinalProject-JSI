1ffc44dc5c9ce46da2dac250c52f7be58f60749c
nqAn0509 <nguyenquocan597@gmail.com>
2024-01-21 09:06:11 +0700

    api commit

---
+++ js/api.js
@@ -0,0 +1,74 @@
+let product = [
+    {
+        id: 1,
+        name: 'Giày Air Jordan 3 Retro',
+        image:'../assets/Air3.webp',
+        price: '14.890.000₫',
+    },
+    {
+        id: 2,
+        name: 'Giày adidas Samba OG J ‘White Black Gum’ IE3675',
+        image:'../assets/AddidasSamba.webp',
+        price: '1.790.000₫',
+    },
+    {
+        id: 3,
+        name: 'Giày Air Jordan 1 Low GS ‘White Toe’ 553560-063',
+        image:'../assets/Save1.webp',
+        price: '3.490.000₫',
+    },
+    {
+        id: 4,
+        name: 'Giày adidas Samba OG ‘White Green’ ID0440',
+        image:'../assets/AdidasSambaOG.webp',
+        price: '2.890.000₫',
+    },
+    {
+        id: 5,
+        name: 'Giày adidas Samba OG ‘White Green’ ID0440',
+        image:'../assets/AdidasSambaOG.webp',
+        price: '2.890.000₫',
+    },
+    {
+        id: 6,
+        name: 'Giày Air Jordan 1 Retro High OG ‘Celtics’ 555088-315',
+        image:'../assets/Air1Retro.webp',
+        price: '16.690.000₫',
+    },
+    {
+        id: 7,
+        name: 'Giày Air Jordan 1 Retro High OG NRG ‘Not For Resale’ 861428-106',
+        image:'../assets/Air1RetroHigh.webp',
+        price: '24.990.000₫',
+    },
+    {
+        id: 8,
+        name: 'Giày (WMNS) Air Jordan 1 Mid ‘Magenta’ BQ6472-501',
+        image:'../assets/Air1Mid.webp',
+        price: '4.490.000₫',
+    },
+    {
+        id: 9,
+        name: ' Giày adidas Court Spec 2 Tennis Shoes ‘Black’ ID2471',
+        image:'../assets/AdidasTennisShoes.webp',
+        price: '1.690.000₫',
+    },
+    {
+        id: 10,
+        name: ' Giày adidas Pureboost Clima F36637',
+        image:'../assets/AdidasPureboostClima.webp',
+        price: '2.090.000₫',
+    },
+    {
+        id: 11,
+        name: ' Giày Nike Air Zoom Pegasus 39 ‘Black Chlorine Blue’ DH4071-002',
+        image:'../assets/NikeAirZoom.webp',
+        price: '1.890.000₫',
+    },
+    {
+        id: 12,
+        name: 'Giày New Balance 550 ‘White Surf’ BB550SSC',
+        image:'../assets/NewBalance.webp',
+        price: '2.890.000₫',
+    },
+]
